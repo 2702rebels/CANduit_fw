8502839323782e2f74749938757d4b01bbd08d62
Camleaf <bdaace09@gmail.com>
2026-02-10 21:25:05 -0500

    add captiveportal file

--- src/device/device.h
+++ src/device/device.h
@@ -1,5 +1,8 @@
 #ifndef DEVICE_H
-#define DEVICE_H 
+#define DEVICE_H
+
+#define WIFI true
+
 
 #include "Preferences.h"
 
@@ -10,5 +13,7 @@ void setDisabled(bool val);
 
 void setDeviceNum(int id);
 int getDeviceNum();
+
+
 
 #endif
