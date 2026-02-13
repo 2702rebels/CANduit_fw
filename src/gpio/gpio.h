4a297b93c88df6fa6c1e38b869290713fc3a2f97
Camleaf <bdaace09@gmail.com>
2026-02-12 19:57:31 -0500

    start implementing I2C

--- src/gpio/gpio.h
+++ src/gpio/gpio.h
@@ -1,6 +1,9 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include "array";
+
+
 const int GPIO[] = {4, 7, 17, 19, 9, 11, 14, 2};
 const int PWR[] = {5, 15, 18, 3, 10, 13, 45, 1};
 const int DIR[] = {6, 16, 8, 20, 46, 12, 48, 38};
@@ -14,8 +17,8 @@ constexpr struct {
     const int UNASSIGNED = 0;
     const int DIG_IN = 1;
     const int DIG_OUT = 2;
-    const int PWM_OUT = 3;
-    const int PWM_IN = 4;
+    const int PWM_IN = 3;
+    const int I2C = 4; // Signifies that is in I2C mode. Shouldn't be able to set to this
 } GPIOMode;
 
 const int modesCount = 5;
@@ -46,5 +49,30 @@ extern volatile uint32_t period[8];
 extern volatile uint32_t highTime[8];
 extern volatile uint32_t lowTime[8];
 
+
+// Setup I2C class
+// I2C itself not implemented yet just laying framework
+
+class I2CMaster {
+    public:
+        I2CMaster(int SDA,int SCL);
+        Port* SDA;
+        Port* SCL;
+        
+        //std::array<uint8_t,8> request();
+        //void transmit(String msg);
+        void destroy();
+};
+
+class I2CSlave {
+    public:
+        I2CSlave(int SDA,int SCL);
+        Port* SDA;
+        Port* SCL;
+        
+        //void onRequest();
+        //void onRecieve();
+        void destroy();
+};
 
 #endif
